775a77d1eb2dda34f16173ebbf3b480fd108eba3
gusrlLee <gusrl3204@naver.com>
2023-01-01 23:24:51 +0900

    ready develop

---
+++ Source/Engine/Engine.h
@@ -0,0 +1,17 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <thread>
+#include <mutex>
+
+class Engine {};
+
+#endif // ENGINE_H
